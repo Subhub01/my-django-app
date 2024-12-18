a6b001b01167ea81f489056e0c754173cc4b9cdd
Git-sub1 <hello-sub1@berkeley.edu>
2024-12-18 01:04:39 -0800

    settings change

--- ethics_site/ethics_site/settings.py
+++ ethics_site/ethics_site/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-2h0aot0b*a4kvo58ubk_gzvx_j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['your-app-name.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['ethicsapp.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
